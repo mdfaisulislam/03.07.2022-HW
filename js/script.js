1981fab4ad00925d62d219587260f8ebcdf4651d
Md.Faisul Islam <101509931+mdfaisulislam@users.noreply.github.com>
2022-07-07 18:48:05 +0600

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,52 @@
+// event
+function e_Textchange(){
+
+	document.getElementById("e_text").innerHTML = "Hellow World";
+
+}
+
+function e_Textchange1(){
+
+	document.getElementById("e_text1").style.color = "green";
+
+}
+
+function e_Textchange2(){
+
+	document.getElementById("e_text2").innerHTML = "Hellow World";
+
+}
+
+function e_Textchange3(){
+
+	document.getElementById("e_text3").innerHTML = "Hellow World";
+
+}
+
+function e_Textchange4(){
+
+	document.getElementById("e_text4").innerHTML = "Hellow World";
+
+}
+function e_Textchange5(){
+
+	document.getElementById("e_text5").style.color = "green";
+
+}
+function e_Textchange6(){
+
+	alert("Hellow World");
+
+}
+
+function e_Textchange7(){
+
+	console.log("Hellow World");
+
+}
+function e_Textchange8(){
+
+	document.write("Hellow World");
+
+}
+
